367c4cd86d85eed794f578686ee10476217f6f9b
baytursevval <baytursevval@gmail.com>
2024-12-13 16:02:29 +0300

    PostService olusturma

--- src/main/java/com/sevval/socialapp/entities/Post.java
+++ src/main/java/com/sevval/socialapp/entities/Post.java
@@ -1,23 +1,64 @@
 package com.sevval.socialapp.entities;
 
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 
 @Entity //dbde tabloya map'ler
 @Table(name="post")
-@Data //getter ve setter'ları otomatik ekler
+//@Data //getter ve setter'ları otomatik ekler
 public class Post {
 	
 	@Id
 	Long id;
-	Long userId;
+	
+	//Long userId;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name="user_id", nullable = false)
+	@OnDelete(action = OnDeleteAction.CASCADE) //user silindiğinde postları da silinsin
+	@JsonIgnore
+	User user;
+	
 	String title;
 	@Lob //text alanının içeriği büyük nesne olarak işaretlenir
 	@Column(columnDefinition="text") //hibernate'in mysqlde stringi text olarak algılaması için
 	String text;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	
+	
 
 }
--- src/main/java/com/sevval/socialapp/repos/PostRepository.java
+++ src/main/java/com/sevval/socialapp/repos/PostRepository.java
@@ -1,9 +1,13 @@
 package com.sevval.socialapp.repos;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.sevval.socialapp.entities.Post;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+	List<Post> findByUserId(Long userId);
 
 }
